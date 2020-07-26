58579eb9999dcd4ce399b6e98fdb2b9768d7a105
kenta-nishimoto <kenta.nishimoto@di-v.co.jp>
2020-07-26 11:17:54 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+
+  #上記のPostsがrouteで設定したコントローラー名
+  #下記のindexはroutesで設定したインデックス名
+
+  def index  # indexアクションを定義した
+      @posts = Post.all  # すべてのレコードを@postsに代入
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+
+end
